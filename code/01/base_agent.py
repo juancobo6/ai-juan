c1326a8b23932af1fe929660f5f2dbd681274135
JuanCobo <juan.cobo@baobabsoluciones.es>
2025-05-23 23:17:55 +0200

    01 Introducción a agentes: code

--- code/01/base_agent.py
+++ code/01/base_agent.py
@@ -9,3 +9,9 @@ load_dotenv()
 llm = GeminiModel(model_name="gemini-2.0-flash")
 
 base_agent = Agent(model=llm)
+
+print("Bienvenido al agente base!\n")
+user_input = input("> ")
+
+response = base_agent.run_sync(user_input)
+print(response.output)
